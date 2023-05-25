dc9832517289b2ae65154932a9f68383cb1a512e
aidalaricchia2 <aidalaricchiah2@gmail.com>
2023-05-25 12:28:27 -0300

    primer prueba zeldaZaurus

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//<>
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    private Rigidbody2D rig;
+    private void Awake(){
+        rig=GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate(){
+
+        float horizontal =Input.GetAxis("Horizontal");
+        float vertical =Input.GetAxis("Vertical");
+        rig.velocity= new Vector2(horizontal,vertical)*speed;
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        /*var playerSR = GetComponent<SpriteRenderer>();
+        if (Input.GetKey(KeyCode.D)){
+            if (playerSR.flipX){
+                playerSR.flipX = false;
+            }
+            var currentPos = transform.position;
+            currentPos.x+=speed;       
+            transform.position = currentPos;
+        }
+        if (Input.GetKey(KeyCode.A)){
+            if (!playerSR.flipX){
+                playerSR.flipX = true;
+            }
+            var currentPos = transform.position;
+            currentPos.x-=speed;       
+            transform.position = currentPos;
+        }
+        if (Input.GetKey(KeyCode.W)){
+            var currentPos = transform.position;
+            currentPos.y+=speed;       
+            transform.position = currentPos;
+        }
+        if (Input.GetKey(KeyCode.S)){
+            var currentPos = transform.position;
+            currentPos.y-=speed;       
+            transform.position = currentPos;
+        }
+*/
+    }
+}
